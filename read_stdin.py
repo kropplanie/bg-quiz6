3972cfef028bcd0920c5be23127577a2a406edc4
kropplanie <kropplanie@gmail.com>
2024-10-27 17:41:23 -0400

    Update read_stdin.py

--- read_stdin.py
+++ read_stdin.py
@@ -2,14 +2,30 @@
 
 import sys
 
-# define a function to hash an entry into one of 10 buckets
-def hash_10(input):
-  return hash(input) % 10
+# initialize maximum number of leading 0s
+max_leading = 0
+users_estimate = 1
 
-with open('queries_sample.txt', 'a') as f:
-  while True:
-      line = sys.stdin.readline() # read in the current line
-      if hash_10(line) == 0: # if it is hashed into bucket 0
-        f.write(f"{line.strip()}") # save the query in the sample file
-        f.flush() # write the data to the file immediately
-      print(f"{line.strip()}") # print the query (all queries)
+
+while True:
+    line = sys.stdin.readline()
+    user_pattern = r'sender (\w{8})' # regex pattern for user id
+    match = re.search(user_pattern, line) # use re.search to find the user ID in the line
+    if match:
+        user_id = match.group(1)  # extract user id
+
+    # hash object to its 64 bit representation
+    hash_rep = bin(int.from_bytes(hashlib.sha256(b"wagon").digest(), 'little'))[-64:] # 64-bit
+
+    # count the number of leading 0s
+    leading_0s = next(i for i, e in enumerate(hash_rep + '1') if e == '1')
+
+    # check if we have a new highest number of leading zeros and update if necessary
+    if leading_0s > max_leading:
+      max_leading = leading_0s # record new maximum
+      users_estimate = 2**max_leading
+    
+    
+    print(f"{line.strip()}")
+    print(f"estimated number of users: {users_estimate})
+

6556deaeb0e94a952b0c03aae9c53415dcc42181
kropplanie <kropplanie@gmail.com>
2024-10-27 19:23:58 -0400

    Update click-feeder.py

--- click-feeder.py
+++ click-feeder.py
@@ -9,12 +9,12 @@ from click_gen import *
 max_evs = 6000         # Total number of events that will be generated. 
                        # If you want to make it run forever, set this number to like a million
 
-n_senders = 1000          # Number of senders
+n_senders = 500          # Number of senders
 n_queries = 60         # How many different queries (or messages) a sender can send
 
 dispatcher = Dispatcher()
 sender_names = shuffle(['sndr%04d' % i for i in range(n_senders)])
-senders = [dispatcher.add_sender(Sender(sender_name, n_queries, n_senders*0.2, n_senders*0.2)) for sender_name in sender_names]
+senders = [dispatcher.add_sender(Sender(sender_name, n_queries, n_senders*0.1, n_senders*0.1)) for sender_name in sender_names]
 
 for ev in dispatcher.launch():
     print(ev, flush=True)

d9f30ae94a4b00369d0687da7a85499d0494f86f
kropplanie <kropplanie@gmail.com>
2024-10-27 18:46:00 -0400

    Update read_stdin.py

--- read_stdin.py
+++ read_stdin.py
@@ -17,16 +17,14 @@ while True:
         user_id = match.group(1)  # extract user id
 
         hash_value = mmh3.hash(user_id, 42, signed=False) & ((1 << 32)-1)
-        hash_int = int(hash_value, 16)
-        bin_hash = bin(hash_int)[2:]  # convert to binary
-        rev_bin_hash = bin_hash[::-1]
+        rev_hash_value = hash_value[::-1]
         
         # count the number of leading 0s
-        leading_0s = next(i for i, e in enumerate(rev_bin_hash + '1') if e == '1')
+        leading_0s = next(i for i, e in enumerate(rev_hash_value + '1') if e == '1')
 
         # check if we have a new highest number of leading zeros and update if necessary
         if leading_0s > max_leading:
-            print(f'new max leading zeros hash: {user_id}, {rev_bin_hash}')
+            print(f'new max leading zeros hash: {user_id}, {rev_hash_value}')
             max_leading = leading_0s # record new maximum
             users_estimate = 2**max_leading
     

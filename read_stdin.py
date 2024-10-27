5607f17334b6655dc2239ffce9618a559b3eb8b4
kropplanie <kropplanie@gmail.com>
2024-10-27 18:48:57 -0400

    Update read_stdin.py

--- read_stdin.py
+++ read_stdin.py
@@ -5,7 +5,7 @@ import re
 import mmh3
 
 # initialize maximum number of leading 0s
-max_leading = 0
+max_trailing = 0
 users_estimate = 1
 
 
@@ -16,17 +16,15 @@ while True:
     if match:
         user_id = match.group(1)  # extract user id
 
-        hash_value = mmh3.hash(user_id, 42, signed=False) & ((1 << 32)-1)
-        rev_hash_value = hash_value[::-1]
-        
+        hash_value = mmh3.hash(user_id, 42, signed=False) & ((1 << 32)-1)        
         # count the number of leading 0s
-        leading_0s = next(i for i, e in enumerate(rev_hash_value + '1') if e == '1')
+        trailing_0s = next(i for i, e in enumerate(reversed(rev_hash_value)) if e == '1')
 
         # check if we have a new highest number of leading zeros and update if necessary
-        if leading_0s > max_leading:
+        if trailing_0s > max_leading:
             print(f'new max leading zeros hash: {user_id}, {rev_hash_value}')
-            max_leading = leading_0s # record new maximum
-            users_estimate = 2**max_leading
+            max_trailing = trailing_0s # record new maximum
+            users_estimate = 2**max_trailing
     
     
     print(f"{line.strip()}")

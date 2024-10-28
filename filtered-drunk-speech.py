e0bd2ad321c80de1c588c2d12a32abde11d1a888
kropplanie <kropplanie@gmail.com>
2024-10-28 18:58:59 -0400

    Update filtered-drunk-speech.py

--- filtered-drunk-speech.py
+++ filtered-drunk-speech.py
@@ -4,47 +4,8 @@ import nltk
 import random
 import time
 import pathlib
-import base64
 
 import pandas as pd, numpy as np
-
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import BooleanType
-from pyspark.sql.functions import col, unbase64
-
-# initialize Spark session
-spark = SparkSession.builder \
-    .appName("BloomFilterCheck") \
-    .getOrCreate()
-
-# path to the Base64-encoded text file in HDFS
-hdfs_path = "hdfs://///user/kroppl/bloom-filter/bloom_filter_encoded.txt" 
-
-# load the Base64-encoded text file into a DataFrame
-encoded_bloom_df = spark.read.text(hdfs_path)
-
-# extract the encoded string
-encoded_bloom = encoded_bloom_df.first()[0]
-
-# decode the Bloom filter
-decoded_bloom = base64.b64decode(encoded_bloom)
-
-# convert the decoded Bloom filter to a list of bits
-bloom_filter_bits = [int(bit) for byte in decoded_bloom for bit in format(byte, '08b')]
-
-# define a function to hash a word and check its membership in the Bloom filter
-def is_in_bloom_filter(word):
-    hash_values = [
-        hash(word + str(i)) % len(bloom_filter_bits)
-        for i in range(3)
-    ]
-    return all(bloom_filter_bits[h] for h in hash_values)
-
-
-# make the function a user defined function
-is_in_bloom_filter_udf = udf(is_in_bloom_filter, BooleanType())
-
 
 afinn_src = 'https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-en-165.txt'
 
@@ -79,18 +40,9 @@ for i in range(1000):
         if len(words) < 3:
             continue
         else:
-            # before printing, check against the bloom filter and do not print the sentence if any of the words are in the bad words list
-            words_df = spark.createDataFrame([(word,) for word in words], ["word"]) # put words in a spark dataframe
-            result_df = words_df.withColumn("in_bloom_filter", is_in_bloom_filter_udf("word")) # check the filter
-            if result_df.filter(col("in_bloom_filter") == True).count() > 0: # if the filter found any of the bad words in the sentence, do not print sentence
-                continue
-            else:
-                print (' '.join(words), flush=True) # otherwise, print the sentence
+            print (' '.join(words), flush=True)
             time.sleep(random_delay(deltaT))
             sent += 1
             words = []
 
 print ('Sent', sent, 'utterings', flush=True, file=sys.stderr)
-
-# stop the Spark session
-spark.stop()
